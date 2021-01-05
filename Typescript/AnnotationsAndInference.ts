f8b84a135e8769d40cc0933c648891f48eb2afc2
ashishtayal89 <ashishtayal14@gmail.com>
2021-01-05 19:36:37 +0530

    Add typescript basics

---
+++ Typescript/AnnotationsAndInference.ts
@@ -0,0 +1,10 @@
+var any1;
+var num1: number;
+var num2: number = 2;
+var num3 = 3;
+var num4 = num3 + 4;
+var str1 = num4 + "Five";
+// var num5: number = num4 + "Five";
+
+any1 = 1;
+any1 = "";
---
+++ Typescript/annotationsandinference.js
@@ -0,0 +1,9 @@
+var any1;
+var num1;
+var num2 = 2;
+var num3 = 3;
+var num4 = num3 + 4;
+var str1 = num4 + "Five";
+// var num5: number = num4 + "Five";
+any1 = 1;
+any1 = "";
